ab6610b6c3f56d798f36ef6e7f886447e02ecb0b
Paul M. Jones <pmjones88@gmail.com>
2013-05-16 17:26:15 -0500

    release Aura.View 1.1.2

--- _scripts/build-packages-include.php
+++ _scripts/build-packages-include.php
@@ -13,7 +13,7 @@ $packages = [
     'Aura.Signal'   => '1.0.1',
     'Aura.Sql'      => '1.1.0',
     'Aura.Uri'      => '1.1.0',
-    'Aura.View'     => '1.1.1',
+    'Aura.View'     => '1.1.2',
     'Aura.Web'      => '1.0.1',
 ];
 
