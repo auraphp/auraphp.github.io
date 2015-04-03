b7ca4f91eb2f62450f1291cd81524fd1da37587e
Paul M. Jones <pmjones88@gmail.com>
2015-04-03 13:55:48 -0500

    reorder script, link, meta, title, etc

---
+++ js/google.js
@@ -0,0 +1,15 @@
+$(function ($) {
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+
+    // Only enable if the document has a long scroll bar
+    // Note the window height + offset
+    if ( ($(window).height() + 100) < $(document).height() ) {
+        $('#top-link-block').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: {top:100}
+        });
+    }
+}
