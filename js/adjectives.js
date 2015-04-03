88d8e2f1dfd689174f4407c457a31e978ae03118
Paul M. Jones <pmjones88@gmail.com>
2015-04-03 09:50:59 -0500

    limit scope, and add adjectives

--- js/adjectives.js
+++ js/adjectives.js
@@ -1,12 +1,19 @@
-var adjectives = [
-    "powerful",
-    "independent",
-    "sharp",
-    "high-quality",
-    "reliable"
-];
-var adjLower = adjectives[Math.floor(adjectives.length * Math.random())];
-var adjUpper = adjLower[0].toUpperCase() + adjLower.slice(1);
-$('<span />', {
-    html: adjUpper + ' tools for ' + adjLower + ' developers.'
-}).appendTo('#adjectives');
+$(function ($) {
+    var adjectives = [
+        "powerful",
+        "independent",
+        "sharp",
+        "high-quality",
+        "reliable",
+        "serious",
+        "expert-level",
+        "modern",
+        "advanced",
+        "professional"
+    ];
+    var adjLower = adjectives[Math.floor(adjectives.length * Math.random())];
+    var adjUpper = adjLower[0].toUpperCase() + adjLower.slice(1);
+    $('<span />', {
+        html: adjUpper + ' tools for ' + adjLower + ' developers.'
+    }).appendTo('#adjectives');
+});
